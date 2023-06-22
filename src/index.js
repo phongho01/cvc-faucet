2a719079b9db2066dcee9d6f8502f69c1e10b528
phongho01 <phong.ho@napaglobal.com>
2023-06-22 15:03:41 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -8,19 +8,9 @@ const client = createClient();
 //   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 // });
 
-// start().then(async () => {
-//   client.on('error', (err) => console.log('Redis Client Error', err));
-//   await client.connect();
+start().then(async () => {
+  client.on('error', (err) => console.log('Redis Client Error', err));
+  await client.connect();
 
-//   login(client);
-// });
-
-const { ethers } = require('ethers');
-
-const getAccountNonce = async () => {
-  const provider = new ethers.providers.JsonRpcProvider(process.env.KURA_RPC_URL);
-  const nonce = await provider.getTransactionCount(process.env.ACCOUNT_ADDRESS);
-  console.log('nonce', nonce);
-};
-
-getAccountNonce();
+  login(client);
+});
