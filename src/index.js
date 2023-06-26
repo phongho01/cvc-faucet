aa3a76dee2634b94b2e282f9fbefec7227124d46
phongho01 <phong.ho@napaglobal.com>
2023-06-26 09:33:04 +0700

    add telegram events

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { createClient } = require('redis');
-const { start, login } = require('./services/discord');
+const discordBot = require('./services/discord');
+const telegramBot = require('./services/telegram');
 
 const client = createClient();
 
@@ -8,9 +9,9 @@ const client = createClient();
 //   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 // });
 
-start().then(async () => {
+discordBot.start().then(async () => {
   client.on('error', (err) => console.log('Redis Client Error', err));
   await client.connect();
-
-  login(client);
+  discordBot.login(client);
+  telegramBot.start();
 });
---
+++ src/services/telegram.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const token = process.env.TELEGRAM_BOT_API_KEY;
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(token, { polling: true });
+const { isAddress, getBalance, formatBalance } = require('../utils/ethers');
+
+const telegramBot = {
+  start() {
+    console.log('Starting telegram bot...');
+    bot.onText(/\/balance(.*)/, async (msg, match) => {
+      const chatId = msg.chat.id;
+      let res = '';
+
+      let address = match[1].trim();
+
+      if (!address || address === 'faucet') {
+        address = process.env.ACCOUNT_ADDRESS;
+      }
+
+
+
+      if (!isAddress) {
+        res = 'Address is not valid';
+      } else {
+        const balance = await getBalance(address);
+        res = `Balance of ${address}: ${formatBalance(balance)} ${process.env.KURA_SYMBOL}`;
+      }
+
+      bot.sendMessage(chatId, res);
+    });
+  },
+};
+
+module.exports = telegramBot;
