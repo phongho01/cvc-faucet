71b2cb6c7da98662363924770b912713a1e87dae
htphong01 <thanhphonga4.hbt@gmail.com>
2023-07-22 18:40:10 +0700

    change logs file

--- src/utils/logs.js
+++ src/utils/logs.js
@@ -2,12 +2,10 @@ const fs = require('fs');
 
 const writeDiscordLogs = (interaction) => {
   const user = interaction.user;
-  const logsText = `[${new Date().toLocaleString()}] [INFO]: user=${user.username}#${user.discriminator} 
-  channelId=${interaction.channelId} 
-  guildId=${interaction.guildId}
-  guildName=${interaction.member.guild.name}
-  roles=${interaction.member.roles.cache.toString()}
-  `;
+  const roles = JSON.stringify(interaction.member.roles.cache.map((item) => item.name)) || '';
+  const logsText = `[${new Date().toLocaleString()}] [INFO]: user=${user.username}#${user.discriminator}; channelId=${
+    interaction.channelId
+  }; guildId=${interaction.guildId}; guildName=${interaction.member.guild.name}; roles=${roles}`;
   fs.appendFileSync('logs/discord.log', `${logsText}\n`);
 };
 
