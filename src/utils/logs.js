95ec285e651117bc3c5a9d4b93cdaca3787cf76f
phongho01 <phong.ho@napaglobal.com>
2023-09-18 09:27:16 +0700

    fix append file

--- src/utils/logs.js
+++ src/utils/logs.js
@@ -7,7 +7,10 @@ const writeDiscordLogs = (interaction) => {
   const logsText = `[${new Date().toLocaleString()}] [INFO]: user=${user.username}#${user.discriminator}; channelId=${
     interaction.channelId
   }; guildId=${interaction.guildId}; guildName=${guildName}; roles=${roles}`;
-  fs.appendFile('logs/discord.log', `${logsText}\n`);
+  fs.appendFile('logs/discord.log', `${logsText}\n`, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
 };
 
 module.exports = {
