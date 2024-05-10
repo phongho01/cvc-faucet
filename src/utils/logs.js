cdc4002295df07453f1836e4fedc28ba4581d469
Thanh-Phong Ho <thanhphonga4.hbt@gmail.com>
2024-05-10 15:28:29 +0700

    add check role

--- src/utils/logs.js
+++ src/utils/logs.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 
 const writeDiscordLogs = (interaction) => {
   const user = interaction.user || interaction.author;
-  const roles = JSON.stringify(interaction.member.roles.cache.map((item) => item.name)) || 'N/A';
-  const guildName = interaction.member.guild.name || '';
+  const roles = JSON.stringify(interaction?.member?.roles?.cache?.map((item) => item.name)) || 'N/A';
+  const guildName = interaction?.member?.guild?.name || '';
   const logsText = `[${new Date().toLocaleString()}] [INFO]: user=${user.username}#${user.discriminator}; channelId=${
     interaction.channelId
   }; guildId=${interaction.guildId}; guildName=${guildName}; roles=${roles}`;
